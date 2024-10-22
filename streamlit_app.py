a6f8e7a0d420de4aec8e2ea8cd032ba90e83357b
Meux123 <69673789+Meux123@users.noreply.github.com>
2024-10-22 15:59:23 +0000

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import geojson
 
+
+
+with open('local-authority-district.geojson') as f:
+    gj=geojson.load(f)
+
+st.write(gj)
 st.title("🎈 My new app")
 st.write(
 '''hello_world''')
