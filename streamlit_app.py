5333c951a7d6a6dd1612aa34a4bdd310c874b2aa
Meux123 <69673789+Meux123@users.noreply.github.com>
2024-10-22 16:22:15 +0000

    testgraph

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,40 @@
 import streamlit as st
 import geojson
+import pandas as pd
+import plotly.express as px
 
 
+@st.cache_data
+def create_la_geojson():
+    with open('Local_Authority_District.geojson') as f:
+        gj=geojson.load(f)
+    example_la_data = []
+    
+    for i in range(len(gj["features"])):
+        # Extract local authority name
+        la = gj["features"][i]['properties']['LAD21NM']
+        # Assign the local authority name to a new 'id' property for later linking to dataframe
+        gj["features"][i]['id'] = la
+        # While I'm at it, append local authority name to a list to make some dummy data to test, along with i for a value to test on map
+        example_la_data.append([la,i])
+    
+    test_df=pd.DataFrame(example_la_data)
+    test_df.columns=['Local_Authority','Value']
+    return gj,test_df
+gj,test_df=create_la_geojson()
 
-with open('local-authority-district.geojson') as f:
-    gj=geojson.load(f)
+fig = px.choropleth_mapbox(test_df,
+                           geojson=gj,
+                           locations='Local_Authority',
+                           color='Value',
+                           featureidkey="properties.LAD21NM",
+                           color_continuous_scale="Viridis",
+                           mapbox_style="carto-positron",
+                           center={"lat": 55.09621, "lon": -4.0286298},
+                           zoom=4.2,
+                           labels={'val':'value'})
 
-st.write(gj['features'][1])
-st.title("🎈 My new app")
-st.write(
-'''hello_world''')
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+st.title("Example Local Authority Map")
+st.plotly_chart(fig)
